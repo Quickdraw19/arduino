247d2e7a1977bfbd00078b565b7fa8c289c997f2
Larry <larry@qdraw.net>
2023-05-03 20:32:14 -0600

    Update main_template.cpp

--- main_template.cpp
+++ main_template.cpp
@@ -5,9 +5,17 @@
 // Put your setup code here, to run once:
 void setup() {
   debug_init();
+
+  // Test example
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // Put your main code here, to run repeatedly:
 void loop() {
   
+  // Test example
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
