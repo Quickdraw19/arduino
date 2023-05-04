e11c430203dc1dbb2fd273eb8dcb71a80de640f6
Larry <larry@qdraw.net>
2023-05-03 20:20:19 -0600

    Create main_template.cpp

---
+++ main_template.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include "avr8-stub.h"
+#include "app_api.h"
+
+// Put your setup code here, to run once:
+void setup() {
+  debug_init();
+}
+
+// Put your main code here, to run repeatedly:
+void loop() {
+  
+}
